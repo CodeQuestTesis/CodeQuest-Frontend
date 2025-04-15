4d2eabb2c929164f174c52cb456745cf70f2c5bf
Flavio Villacorta <143514047+Flavicox18@users.noreply.github.com>
2025-04-15 02:29:09 -0500

    Ajustando inicio

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import './App.css'
 import PrincipalScene from "./features/tour/PrincipalScene.tsx";
+import HomePage from "./presentation/pages/HomePage.tsx";
+import {useState} from "react";
 
 function App() {
 
-  return (
-      <div className="w-full h-full">
-          <PrincipalScene/>
-      </div>
-  )
+    const [hasStarted, setHasStarted] = useState(false)
+
+    const handleStart = () => {
+        setHasStarted(true)
+    }
+
+    return (
+        <div className="relative w-full h-screen">
+            <PrincipalScene />
+            {!hasStarted && <HomePage onStart={handleStart} />}
+        </div>
+    )
+
 }
 
 export default App
--- src/presentation/pages/HomePage.tsx
+++ src/presentation/pages/HomePage.tsx
@@ -1,39 +1,28 @@
-import {Button} from "../components/ui/Button.tsx";
 import {useState} from "react";
 import {FaQuestion} from "react-icons/fa";
 import HelpModal from "../components/modals/HelpModal.tsx";
+import {Button} from "../components/UI/Button.tsx";
 
-function HomePage() {
-
+function HomePage({ onStart }: { onStart: () => void }) {
     const [modalOpen, setModalOpen] = useState(false);
 
-    const openModal = () => {
-        setModalOpen(true);
-    };
-
-    const closeModal = () => {
-        setModalOpen(false);
-    };
-
     return (
-        <div className="relative flex flex-col items-center justify-center w-full h-screen bg-black/80">
-            {/* Contenido principal centrado */}
+        <div className="absolute z-50 top-0 left-0 w-full h-screen bg-black/80 flex flex-col items-center justify-center">
             <div className="text-center">
                 <h1 className="text-white text-4xl mb-4">Bienvenido a CodeQuest</h1>
-                <Button>Comenzar</Button>
+                <Button onClick={onStart}>Comenzar</Button>
             </div>
 
-            {/* Botón con ícono de pregunta en la esquina superior derecha */}
             <button
-                onClick={openModal}
+                onClick={() => setModalOpen(true)}
                 className="absolute top-4 right-4 flex items-center justify-center w-10 h-10 rounded-full border-2 border-red-800 text-red-800 shadow hover:bg-red-800 hover:text-white transition-colors"
                 aria-label="Abrir ayuda">
                 <FaQuestion />
             </button>
-            {/* Llamada al componente HelpModal */}
-            {modalOpen && <HelpModal onClose={closeModal} />}
+
+            {modalOpen && <HelpModal onClose={() => setModalOpen(false)} />}
         </div>
-    );
+    )
 }
 
 export default HomePage;
